593fe8b6a21afde25db81c1177b990931a06ec60
gwkimgw <marianaaquaria@gmail.com>
2022-09-17 10:49:00 +0900

    Initial commit

---
+++ routes/user.js
@@ -0,0 +1,52 @@
+const express = require('express');
+
+const { isLoggedIn } = require('./middlewares');
+const User = require('../models/user');
+
+const router = express.Router();
+
+router.post('/:id/follow', isLoggedIn, async (req, res, next) => {
+  try {
+    const user = await User.findOne({ where: { id: req.user.id } });
+    if (user) {
+      await user.addFollowing(parseInt(req.params.id, 10));
+      res.send('success');
+    } else {
+      res.status(404).send('no user');
+    }
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.post('/:id/unfollow', isLoggedIn, async (req, res, next) => {
+  try {
+    const user = await User.findOne({ where: { id: req.user.id } });
+    if (user) {
+      await user.removeFollowing(parseInt(req.params.id, 10));
+      //follow table에서 user.id를 찾아 삭제
+      res.send('success');
+    } else {
+      res.status(404).send('no user');
+    }
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.post('/profile', async(req, res, next) => {
+  try{
+    await User.update({nick:req.body.nick}, {
+      where: {id:req.user.id},
+    //user.id를 입력한 body.nick으로 update
+    });
+    res.redirect("/profile");
+  } catch(error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+module.exports = router;
